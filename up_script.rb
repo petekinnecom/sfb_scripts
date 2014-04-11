f17d1744cae42062c22b1de820e21af9100cb7a9
Pete Kinnecom <pete.kinnecom@appfolio.com>
2014-04-11 15:06:59 -0700

    Classify things

--- up_script.rb
+++ up_script.rb
@@ -1,114 +1,168 @@
 #!/usr/bin/env ruby
 
-require 'pry'
+class Shell
+  CommandFailureError = Class.new(StandardError)
 
-def rebase_on_master
-  rebase_origin_master
-  perform_actions
-end
+  attr_accessor :working_directory
 
-def up_master
-  move_to_master
-  pull_origin_master
-  perform_actions
-end
-
-def move_to_master
-  run "git checkout master"
-end
-
-def reset_hard_origin_master
-  run "git reset --hard origin/master"
-end
-
-def pull_origin_master
-  git_up do
-    fetch_origin
-    reset_hard_origin_master
+  def initialize(working_directory)
+    @working_directory = working_directory
   end
-end
 
-def rebase_origin_master
-  git_up do
-    run "git pull --rebase origin master"
-  end
-end
-
-def git_up
-  @old_sha = get_current_sha
-  yield
-  @new_sha = get_current_sha
-end
-
-def get_current_sha
-  run "git rev-parse HEAD"
-end
-
-def fetch_origin
-  run 'git fetch origin'
-end
-
-def perform_actions
-  if @old_sha == @new_sha
-    puts 'No actions required'
-  else
-    bundle_where_necessary
-    migrate_where_necessary
-  end
-end
-
-def run(cmd, working_directory: repo_root)
-  %x{ cd #{working_directory} && #{cmd} }
-end
-
-def repo_root
-  @repo_root ||= %x[ git rev-parse --show-toplevel ].chomp
-end
-
-def bundle_where_necessary
-  find("Gemfile.lock").each do |gemfile_lock|
-    if has_changed?(gemfile_lock)
-      bundle(directory_of(gemfile_lock))
+  def run(cmd, dir: working_directory)
+    command = "cd #{dir} && #{cmd}"
+    %x{ #{command} }.tap do
+      raise CommandFailureError, "The following command has failed: #{command}" if ($?.exitstatus != 0)
     end
   end
-end
 
-def bundle(gemfile_directory)
-  puts "bundle install --local in #{gemfile_directory}"
-  puts (run "bundle install --local", working_directory: gemfile_directory)
-  if $?.exitstatus != 0
-    puts 'trying without --local'
-    puts(run "bundle install", working_directory: gemfile_directory)
+  def stream(*args)
+    puts run(*args)
   end
 end
 
-def has_changed?(file_path)
-  files.changed.include? file_path
+class Repo
+
+  attr_reader :shell
+
+  def initialize(shell: shell)
+    @shell = shell
+  end
+
+  def rebase_on_master!
+    rebase_origin_master
+  end
+
+  def up_master!
+    move_to_master!
+    pull_origin_master!
+  end
+
+  def files_changed
+    @files_changed ||= (shell.run "git diff --name-only #{@old_sha}").split("\n")
+  end
+
+  def changed?(file_path)
+    files_changed.include? file_path
+  end
+
+  private
+
+  def up
+    @old_sha = current_sha
+    yield
+    @new_sha = current_sha
+  end
+
+  def pull_origin_master!
+    up do
+      fetch_origin
+      reset_hard_origin_master!
+    end
+  end
+
+  def rebase_origin_master!
+    up do
+      shell.run "git pull --rebase origin master"
+    end
+  end
+
+  def fetch_origin
+    shell.run 'git fetch origin'
+  end
+
+  def reset_hard_origin_master!
+    shell.run "git reset --hard origin/master"
+  end
+
+  def move_to_master!
+    shell.run "git checkout master"
+  end
+
+  def current_sha
+    shell.run "git rev-parse HEAD"
+  end
+
 end
 
-def find(file_name)
-  Dir.glob("**/#{file_name}")
+class BundleManager
+  attr_accessor :shell, :repo
+
+  def initialize(repo: raise, shell: raise)
+    @shell = shell
+    @repo = repo
+  end
+
+  def bundle_where_necessary
+    find("Gemfile.lock").each do |gemfile_lock|
+      if repo.changed?(gemfile_lock)
+        bundle(directory_of(gemfile_lock))
+      end
+    end
+  end
+
+  def bundle(gemfile_directory)
+    puts "bundle install --local in #{gemfile_directory}"
+    begin
+      shell.stream "bundle install --local", dir: gemfile_directory
+    rescue Shell::CommandFailureError
+      puts 'trying without --local'
+      shell.stream "bundle install", dir: gemfile_directory
+    end
+  end
+
+  def find(file_name)
+    Dir.glob("**/#{file_name}")
+  end
+
+  def directory_of(file_path)
+    File.dirname(file_path)
+  end
+
 end
 
-def directory_of(file_path)
-  File.dirname(file_path)
+class Migrator
+  attr_accessor :shell, :repo
+
+  def initialize(repo: raise, shell: raise)
+    @shell = shell
+    @repo = repo
+  end
+
+  def migrate_where_necessary
+    directories_to_migrate.each do |dir|
+      puts "bundle exec rake db:migrate in #{dir}"
+      shell.stream "bundle exec rake db:migrate", dir: dir
+      shell.run "RAILS_ENV=test bundle exec rake db:migrate", dir: dir
+    end
+  end
+
+  def directories_to_migrate
+    repo.files_changed.select {|f| f.match("/migrate/") }.map {|f| File.dirname(f) }.uniq
+  end
+
 end
 
-def migrate_where_necessary
-  directories_to_migrate.each do |dir|
-    puts "bundle exec rake db:migrate in #{dir}"
-    run "bundle exec rake db:migrate", working_directory: dir
-    run "RAILS_ENV=test bundle exec rake db:migrate", working_directory: dir
+class Upper
+  attr_reader :shell, :repo, :bundler, :migrator
+
+  def initialize(root_dir)
+    Dir.chdir(root_dir)
+    @shell = Shell.new(root_dir)
+    @repo = Repo.new(shell: @shell)
+    @bundler = BundleManager.new(shell: @shell, repo: @repo)
+    @migrator = Migrator.new(shell: @shell, repo: @repo)
+  end
+
+  def up_master
+    repo.up_master!
+    bundler.bundle_where_necessary
+    migrator.migrate_where_necessary
   end
 end
 
-def directories_to_migrate
-  files_changed.select {|f| f.match("/migrate/") }.map {|f| File.dirname(f) }.uniq
+def root_dir
+  @root_dir ||= %x[ git rev-parse --show-toplevel ].chomp
 end
 
-def files_changed
-  @files_changed ||= (run "git diff --name-only #{@old_sha}").split("\n")
-end
-
-Dir.chdir(repo_root)
-up_master
+Upper.new(root_dir).up_master
