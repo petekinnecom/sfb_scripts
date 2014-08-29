00cdd094e4c8cccb2971e4ba971faf70f2f15859
Pete Kinnecom <pete.kinnecom@appfolio.com>
2014-08-28 19:13:11 -0700

    Use Ruby 2.1.2

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'pry'
+gem 'minitest'
 ruby '2.1.2'
+
--- test/test.sh
+++ test/test.sh
@@ -16,25 +16,25 @@ rvm @global do gem install sfb_scripts-${__VERSION}.gem
 cd test/test_app
 
 #test_runner
-git reset --hard 8d2b16a1ffbd3687e5ccef98fadbcdbf4548bd61 > /dev/null
+git reset --hard e2754e5449e66157590e5c8694c36f9843df114c > /dev/null
 run_test `test_runner find test_one | grep test_one_success | wc -l`  1
 
 
 run_test `test_runner find thing_test | grep thing_test_success | wc -l` 1
 
-git reset --hard aa0e2d230903ce5d0afa946e25253865f7874a4c > /dev/null
+git reset --hard fad523ac3479099dda2e16dc4ac642e6e991a751 > /dev/null
 run_test `test_runner find multiple | grep multiple_matches_success | wc -l` 2
 
-git reset --hard 49a28f1c7715403765c9b03e647dde146feed64b > /dev/null
+git reset --hard c7e5f59b0a11337134f53b1252641300e2cfd34d > /dev/null
 git reset @~1
 git add --all
 run_test `test_runner status | grep test_status_success | wc -l` 1
 
 #app_up
-git reset --hard 35250b7b73024a281693bffcb132c7fce4bf7148 > /dev/null
+git reset --hard 1392ec653a68e9566cd8d0c39cc8d6a192932576 > /dev/null
 run_test `app_up --no-git | egrep 'bundle_me|migrate_me' | wc -l` 3
 
-git reset --hard aa0e2d230903ce5d0afa946e25253865f7874a4c > /dev/null
+git reset --hard fad523ac3479099dda2e16dc4ac642e6e991a751 > /dev/null
 run_test `app_up | egrep 'bundle_me|migrate_me' | wc -l` 3
 
 git reset --hard origin/master
